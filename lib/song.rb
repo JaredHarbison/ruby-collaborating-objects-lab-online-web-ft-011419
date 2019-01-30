c07a89cc6ab954ad93223f568af0de127362f79f
Jared Harbison <jared.harbison@gmail.com>
2019-01-30 18:50:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,8 +9,8 @@ class Song
   
   def self.new_by_filename(filename)
     split_name = filename.split(" - ")
-    song = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
+    song = self.new(split_name[0])
+    artist = Artist.find_or_create_by_name(split_name)
     artist.add_song(song)
   end
   
