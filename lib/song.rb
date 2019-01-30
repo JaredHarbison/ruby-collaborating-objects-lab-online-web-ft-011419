2965f44da03332e10a5981efb303408af58c5fc3
Jared Harbison <jared.harbison@gmail.com>
2019-01-30 18:48:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,14 +7,6 @@ class Song
     @name = name 
   end 
   
-  def self.new_by_filename(file_name)
-   artist_and_song = file_name.split(" - ")
-   artist = Artist.new(artist_and_song[0]).save
-   song = self.new(artist_and_song[1])
-   song.artist = artist
-   song 
-  end 
-  
     def self.new_by_filename(filename)
     artist_name, song_name, extra = filename.split(" - ")
     song = self.new(song_name)
