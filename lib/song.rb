a4a442c79eea4a8f0cbcf11cdb40c00be6695c55
Jared Harbison <jared.harbison@gmail.com>
2019-01-30 18:41:21 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+
+class Song 
+  attr_accessor :name, :artist
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def self.new_by_filename(file_name)
+   artist_and_song = file_name.split(" - ")
+   artist = Artist.new(artist_and_song[0]).save
+   song = self.new(artist_and_song[1])
+   song.artist = artist
+   song 
+  end 
+  
+end 
